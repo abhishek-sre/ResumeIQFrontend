647b82add7d27d8cfd308320a6f19c82adc9916e
Abhishek <abhi.jkit@gmail.com>
2026-09-13 17:58:19 +0530

    newsroom module

--- src/services/aiRequestApi.js
+++ src/services/aiRequestApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const sendRequestForAi = async (data) => {
     try {
         const response = await axios.post(
-            "http://localhost:3000/api/ai/resume/analyser",
+            "https://resume-iq-backend.vercel.app/api/ai/resume/analyser",
             data
         )
         return response.data;
